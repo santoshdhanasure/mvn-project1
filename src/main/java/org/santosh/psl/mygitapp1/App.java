a3d5f0b7a41a6bfdcbb4a726c206a463c2ce4a10
santoshdhanasure <santosh_dhanasure@persistent.co.in>
2016-07-05 13:29:49 +0530

    My maven app

---
+++ src/main/java/org/santosh/psl/mygitapp1/App.java
@@ -0,0 +1,13 @@
+package org.santosh.psl.mygitapp1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
